43f9ae38926c111c4afc7ff9199b7323d3e915f9
Zain Dewsi <zain.dewsi@gmail.com>
2024-01-03 10:50:45 -0500

    fresh start

--- src/templates/single-page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function singlePage() {
-    return <h1>Hello World!</h1>;
-}
--- src/utils/store.ts
+++
@@ -1,9 +0,0 @@
-import vegemite from "vegemite"
-
-interface EventMap {
-    "theme:change": undefined;
-}
-
-const store = vegemite<EventMap, {}>();
-
-export default store;
